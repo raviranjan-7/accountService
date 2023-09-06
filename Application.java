a97b4482d8c86d659e25f9a4f92c3e2cc47b39e0
Ravi ranjan <104708229+xml-7@users.noreply.github.com>
2023-09-06 15:27:25 +0530

    Add files via upload

---
+++ Application.java
@@ -0,0 +1,30 @@
+package com.learning;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+
+@RestController
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+	@GetMapping("/")
+	public String defaultMapping() {
+
+		return "Welcome to Java programming";
+	}
+
+	@GetMapping("/instaPost")
+	public String instaPostMapping() {
+
+		return "Post Data{id=101}";
+
+	}
+
+}
